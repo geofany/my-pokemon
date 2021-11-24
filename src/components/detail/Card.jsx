d2a8a44d8b7daaecb846636ee04675bb9bf13a3f
geofany <172410102040@cs.unej.ac.id>
2021-11-24 17:33:46 +0700

    move modal component

--- src/components/detail/Card.jsx
+++ src/components/detail/Card.jsx
@@ -1,11 +1,11 @@
-import React, { Fragment, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import BtnCatch from './BtnCatch';
 import Type from './Type';
 import Img from './Img';
 import Info from './Info';
 import Moves from './Moves';
 import Title from './Title';
-import { Dialog, Transition } from '@headlessui/react';
+import Modal from './Modal';
 import { GET_POKEMON } from '../../graphql/get-pokemon';
 import { useQuery } from '@apollo/react-hooks';
 import { useParams } from 'react-router';
@@ -45,9 +45,10 @@ function Card() {
   }
 
   function nameHandler(event) {
-    let nickname = myPokemons.find(pokemon => pokemon.nickname === event.target.value)
+    let lowerCase = event.target.value.toLowerCase()
+    let nickname = myPokemons.find(pokemon => pokemon.nickname === lowerCase)
     setDuplicate(nickname ? true : false)
-    setName(event.target.value)
+    setName(lowerCase)
   }
 
   if (data) {
@@ -98,112 +99,17 @@ function Card() {
           </div>
         </div>
 
-        <Transition appear show={isOpen} as={Fragment}>
-          <Dialog
-            static
-            as="div"
-            className="fixed inset-0 z-10 overflow-y-auto"
-            onClose={closeModal}
-          >
-            <div className="min-h-screen px-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 bg-dark-100-30" />
-              </Transition.Child>
-
-              {/* This element is to trick the browser into centering the modal contents. */}
-              <span
-                className="inline-block h-screen align-middle"
-                aria-hidden="true"
-              >
-                &#8203;
-              </span>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <div className="inline-block w-auto p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-dark-60"
-                  >
-                    <p className={`${catched ? '' : 'hidden'}`}>
-                      Success to catch Pokémon!
-                    </p>
-                    <p className={`${catched ? 'hidden' : ''}`}>
-                      Failed to catch Pokémon!
-                    </p>
-                  </Dialog.Title>
-                  <div className="mt-1">
-                    <p className={`${catched ? '' : 'hidden'} text-sm text-gray-90`}>
-                      Give your Pokémon name
-                    </p>
-                    <p className={`${catched ? 'hidden' : ''} text-sm text-gray-90`}>
-                      Don't give up
-                    </p>
-                  </div>
-                  <div className={`${catched ? '' : 'hidden'}`}>
-                    <input
-                      id="name"
-                      name="name"
-                      type="text"
-                      value={name}
-                      className={`border rounded px-2 py-1 focus:outline-none w-full ${duplicate ? 'border-danger-60' : 'border-gray-60 focus:border-gray-90'}`}
-                      onChange={nameHandler}
-                    />
-                    <div>
-                      <small className={`text-danger-60 ${duplicate ? '' : 'hidden'}`}>
-                        Name already exist!
-                      </small>
-                    </div>
-                  </div>
-
-                  <div className="mt-4 flex justify-end">
-                    <button
-                      type="button"
-                      disabled={duplicate ? true : false}
-                      className={`${catched ? '' : 'hidden'} ${duplicate ? 'text-gray-60 bg-gray-10 cursor-not-allowed' : 'text-success-60 bg-success-10 hover:bg-success-20'} inline-flex justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-success-50`}
-                      onClick={() => {
-                        closeModal()
-                        dispatch({
-                          type: 'ADD_POKEMON',
-                          pokemon: {
-                            id: data.pokemon.id,
-                            name: data.pokemon.name,
-                            img: data.pokemon.sprites.front_default,
-                            nickname: name
-                          }
-                        })
-                        setName('')
-                      }}
-                    >
-                      Save!
-                    </button>
-                    <button
-                      type="button"
-                      className={`${catched ? 'hidden' : ''} inline-flex justify-center px-4 py-2 text-sm font-medium text-danger-60 bg-danger-10 border border-transparent rounded-md hover:bg-danger-20 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-danger-50`}
-                      onClick={closeModal}
-                    >
-                      Close
-                    </button>
-                  </div>
-                </div>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition>
+        <Modal
+          dispatch={dispatch}
+          isOpen={isOpen}
+          name={name}
+          catched={catched}
+          duplicate={duplicate}
+          closeModal={closeModal}
+          nameHandler={nameHandler}
+          setName={setName}
+          data={data}
+        />
       </>
     )
   }
---
+++ src/components/detail/Modal.jsx
@@ -0,0 +1,113 @@
+import React, { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react';
+
+export default function Modal(props) {
+  return (
+    <Transition appear show={props.isOpen} as={Fragment}>
+      <Dialog
+        static
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={props.closeModal}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-dark-100-30" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-auto p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <Dialog.Title
+                as="h3"
+                className="text-lg font-medium leading-6 text-dark-60"
+              >
+                <p className={`${props.catched ? '' : 'hidden'}`}>
+                  Success to catch Pokémon!
+                </p>
+                <p className={`${props.catched ? 'hidden' : ''}`}>
+                  Failed to catch Pokémon!
+                </p>
+              </Dialog.Title>
+              <div className="mt-1">
+                <p className={`${props.catched ? '' : 'hidden'} text-sm text-gray-90`}>
+                  Give your Pokémon name
+                </p>
+                <p className={`${props.catched ? 'hidden' : ''} text-sm text-gray-90`}>
+                  Don't give up
+                </p>
+              </div>
+              <div className={`${props.catched ? '' : 'hidden'}`}>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  value={props.name}
+                  className={`border rounded px-2 py-1 focus:outline-none w-full ${props.duplicate ? 'border-danger-60' : 'border-gray-60 focus:border-gray-90'}`}
+                  onChange={props.nameHandler}
+                />
+                <div>
+                  <small className={`text-danger-60 ${props.duplicate ? '' : 'hidden'}`}>
+                    Name already exist!
+                  </small>
+                </div>
+              </div>
+
+              <div className="mt-4 flex justify-end">
+                <button
+                  type="button"
+                  disabled={props.duplicate ? true : false}
+                  className={`${props.catched ? '' : 'hidden'} ${props.duplicate ? 'text-gray-60 bg-gray-10 cursor-not-allowed' : 'text-success-60 bg-success-10 hover:bg-success-20'} inline-flex justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-success-50`}
+                  onClick={() => {
+                    props.closeModal()
+                    props.dispatch({
+                      type: 'ADD_POKEMON',
+                      pokemon: {
+                        id: props.data.pokemon.id,
+                        name: props.data.pokemon.name,
+                        img: props.data.pokemon.sprites.front_default,
+                        nickname: props.name
+                      }
+                    })
+                    props.setName('')
+                  }}
+                >
+                  Save!
+                </button>
+                <button
+                  type="button"
+                  className={`${props.catched ? 'hidden' : ''} inline-flex justify-center px-4 py-2 text-sm font-medium text-danger-60 bg-danger-10 border border-transparent rounded-md hover:bg-danger-20 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-danger-50`}
+                  onClick={props.closeModal}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
